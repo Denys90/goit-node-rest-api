64c2b47f9760e4fb707b1214c58a1175c7033283
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2024-02-23 20:02:41 +0100

    update

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,12 +4,12 @@ export const createContactSchema = Joi.object({
   name: Joi.string().required().min(2).max(16),
   email: Joi.string().email().required(),
   phone: Joi.string().required(),
-  favorite: Joi.bool().default(false),
+  favorite: Joi.boolean().default(false),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().optional().min(2).max(16),
   email: Joi.string().email().optional(),
   phone: Joi.string().optional(),
-  favorite: Joi.bool().default(false),
+  favorite: Joi.boolean().default(false),
 });
